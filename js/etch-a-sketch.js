7760407b04012de281b325b42b41e16d90e5768e
Midhun Pradeep Nair <52858999+midhunpradeep@users.noreply.github.com>
2023-09-02 23:19:32 +0100

    feat: Show grid

--- js/etch-a-sketch.js
+++ js/etch-a-sketch.js
@@ -1,5 +1,38 @@
 "use strict";
 
-function main() {}
+function createGrid(numBoxesX, numBoxesY) {
+  const vw = Math.max(
+    document.documentElement.clientWidth || 0,
+    window.innerWidth || 0,
+  );
+  const vh = Math.max(
+    document.documentElement.clientHeight || 0,
+    window.innerHeight || 0,
+  );
+
+  let canvas = document.querySelector("#canvas");
+  for (let i = 0; i < numBoxesY; i++) {
+    let rowFlexBox = document.createElement("div");
+    rowFlexBox.classList.add("row");
+    canvas.appendChild(rowFlexBox);
+
+    for (let j = 0; j < numBoxesX; j++) {
+      let box = document.createElement("div");
+      box.classList.add("box");
+
+      let idealWidth = vh / numBoxesY;
+      let idealHeight = vw / numBoxesX;
+
+      let actualHeightWidth = Math.min(idealHeight, idealWidth);
+      box.style.minHeight = actualHeightWidth + "px";
+      box.style.minWidth = actualHeightWidth / numBoxesX + "px";
+      rowFlexBox.appendChild(box);
+    }
+  }
+}
+
+function main() {
+  createGrid(16, 16);
+}
 
 main();
